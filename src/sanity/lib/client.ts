1970de7d1eccb5017f4927a740b6f18a94e758f0
Zahida Raees <160402259+zahidaraees@users.noreply.github.com>
2025-02-07 22:01:25 +0500

    updated client data

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -3,8 +3,8 @@ import { createClient } from 'next-sanity'
 import { apiVersion, dataset, projectId } from '../env'
 
 export const client = createClient({
-  projectId,
-  dataset,
-  apiVersion,
-  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,  
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  apiVersion: "2023-01-01",
+  useCdn: false,  // Set to false to ensure fresh data is returned  
 })
